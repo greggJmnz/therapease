137712be5d4493e38936510af7e8ba9743a84151
greggJmnz <ga.jimenez25@gmail.com>
2025-12-01 22:57:28 +0800

    Remove Close button from therapist appointment details modal
    - Removed Close button from modal footer
    - Users can still close modal using X button in header

--- client/src/pages/Therapist/TherapistSchedule.jsx
+++ client/src/pages/Therapist/TherapistSchedule.jsx
@@ -1109,12 +1109,6 @@ const TherapistSchedule = () => {
                       <span className="text-sm">Cannot edit admin-created appointments</span>
                     </div>
                   )}
-                  <button
-                    onClick={handleCloseAppointmentModal}
-                    className="px-6 py-3 border border-gray-300 text-gray-700 rounded-lg hover:bg-gray-50 transition-colors font-medium whitespace-nowrap"
-                  >
-                    Close
-                  </button>
                 </div>
               </div>
             </div>
