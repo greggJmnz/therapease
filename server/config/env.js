13ad5b74e350d6b425b2be1622c9b3bcd82ce661
“lemonCoder0101” <jimenezgregg365@gmail.com>
2026-05-19 10:49:37 +0800

    cors fixed v2

--- server/config/env.js
+++ server/config/env.js
@@ -125,7 +125,7 @@ const getCorsOrigins = () => {
     throw new Error('CORS_ORIGIN is required in production');
   }
 
-  return [frontendUrl, 'https://therapease-ebon.vercel.app/'];
+  return [frontendUrl, 'https://therapease-ebon.vercel.app'];
 };
 
 module.exports = {
